550c36044b23099d503e1dcb31116cac6f63b55b
deven367 <masterdeven@gmail.com>
2022-06-12 11:45:08 -0400

    revert to git url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://deven367.github.io/my-website',
-  baseUrl: '/',
+  baseUrl: '/my-website/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
